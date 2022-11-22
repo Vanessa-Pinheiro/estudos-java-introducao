f5d3ff675d27106d034fb7db8e3be375744f91d5
Vanessa Pinheiro <vanessa05pinheiro@gmail.com>
2022-11-22 00:35:16 -0300

    Commit inicial

---
+++ src/estruturas/java/estruturasCondicionais.java
@@ -0,0 +1,81 @@
+package estruturas.java;
+
+import java.util.Scanner;
+
+// IF
+public class estruturasCondicionais {
+    public static void main(String[] args) {
+        /*int idade = 16;
+        String autorizado = "Qual cerveja vai ser?";
+
+        if (idade > 18) {
+            System.out.println(autorizado);
+        }
+        System.out.println("Não autorizado. Vai refri?");
+
+    }*/
+
+
+        // IF ELSE
+
+
+      /*  int rendaPounpanca = 25000;
+        int metaPoupanca = 20000;
+        double ingressoBeyonce = 12000.00;
+        boolean vouConseguir = rendaPounpanca >= metaPoupanca;
+
+        if (rendaPounpanca >= metaPoupanca) {
+            System.out.println("Get reaaaadyyyy");
+        } else {
+            System.out.println("Keep working on, bitch");
+        }*/
+
+        // ELSE IF
+
+        /*int idade = 24;
+        if (idade <= 15) {
+            System.out.println("Categoria infantial!");
+        } else if (idade >= 15 && idade <=18){
+            System.out.println("Categoria juvenil!");
+        }else {
+            System.out.println("Categoria adulto!");
+        }*/
+
+        //OPERADOR TERNÁRIO
+
+
+
+        // SWITCH
+
+       /*byte dia = 8;
+       switch (dia){
+           case 1:
+               System.out.println("Domingo");
+               break;
+           case 2:
+               System.out.println("Segunda");
+               break;
+           case 3:
+               System.out.println("Terça");
+               break;
+           case 4:
+               System.out.println("Quarta");
+               break;
+           case 5:
+               System.out.println("Quinta");
+               break;
+           case 6:
+               System.out.println("Sexta");
+               break;
+           case 7:
+               System.out.println("Sábado");
+               break;
+           default:
+               System.out.println("Inválida");
+       }*/
+
+
+    }
+}
+
+
